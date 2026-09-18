c26dd9cfd0f035c8cbf4c9e41397276ffc7271fe
BerenYu <yoosc1314@gmail.com>
2026-09-18 09:44:40 +0900

    React 02 JSX

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import JavaScript from './components/JavaScript'
+import Style from './components/Style'
+
+function App() {
+
+  return (
+    <>
+      <h1>JSX</h1>
+      {/* <JavaScript /> */}
+      <Style />
+    </>
+  )
+}
+
+export default App
